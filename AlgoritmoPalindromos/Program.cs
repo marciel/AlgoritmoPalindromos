23df96226a1ccb0360f203788445d588572b29f9
Marciel <marciel.dls@gmail.com>
2024-02-10 11:30:39 -0300

    Algoritmo

---
+++ AlgoritmoPalindromos/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace AlgoritmoPalindromos;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var inputString = "aaa";
+        var qtde = ContaPalindromos(inputString);
+        Console.WriteLine("Qtde palíndromos: " + qtde);
+    }
+
+    static int ContaPalindromos(string s)
+    {
+        int numLetras = s.Length;
+        int contagem = 0;
+
+        for (int centro = 0; centro < 2 * numLetras - 1; centro++)
+        {
+            int esquerda = centro / 2;
+            int direita = esquerda + centro % 2;
+
+            while (esquerda >= 0 && direita < numLetras && s[direita] == s[esquerda])
+            {
+                contagem++;
+                esquerda--;
+                direita++;
+            }
+        }
+        return contagem;
+    }
+}
+
